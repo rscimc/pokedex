95cfdb296b93435c6df36bd9cd540f27a7fd2d8a
Ricardo <rscimc@gmail.com>
2023-08-22 23:01:02 -0300

    Created Pokemon Model

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,16 @@ const pokemonList = document.getElementById("pokemonList")
 function convertPokemonToLi(pokemon) {
     return `
     </li>
-    <li class="pokemon">
-        <span class="number">#001</span>
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
             </ol>
 
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            <img src="${pokemon.photo}"
                 alt="${pokemon.name}">
         </div>
     </li>
